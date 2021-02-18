098fe4aeba41839b3f3ba68c2c726c289da1ada4
Jeremy Botha <jeremy.botha@vonage.com>
2021-02-18 12:16:56 +0000

    reorg

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,41 +26,42 @@ Summary.create(facade: facade, description: "Java, Ruby and DevOps generalist")
 Skill.create(
     [
         {name: 'Java', level: 4, years: 18, tag: '+', major: true, suppress: false},
-        {name: 'Ruby', level: 4, years: 7, major: true, suppress: false},
-        {name: 'Sql', level: 3, years: 18, tag: '+', major: true, suppress: false},
-        {name: 'Jms', level: 3, years: 14, major: true, suppress: false},
+        {name: 'Ruby', level: 4, years: 8, major: true, suppress: false},
+        {name: 'Sql', level: 4, years: 18, tag: '+', major: true, suppress: false},
+        {name: 'Jms', level: 3, years: 14, major: false, suppress: false},
         {name: 'Shell_Script', level: 3, years: 14,  major: true, suppress: true},
         {name: 'Concurrency', level: 3, years: 14, tag: '+', major: true, suppress: true},
         {name: 'Jenkins', level: 3, years: 10, major: true, suppress: true},
-        {name: 'Mysql', level: 3, years: 10, major: true, suppress: true},
+        {name: 'Mysql', level: 3, years: 12, major: true, suppress: true},
         {name: 'SOAP', level: 3, years: 8,  major: true, suppress: true},
-        {name: 'REST', level: 3, years: 6,  major: true, suppress: true},
-        {name: 'Amqp', level: 3, years: 4, major: true, suppress: false},
+        {name: 'REST', level: 3, years: 8,  major: true, suppress: true},
+        {name: 'Amqp', level: 3, years: 4, major: true, suppress: true},
         {name: 'FIX', level: 3, years: 4,  major: true, suppress: false},
-        {name: 'Terraform', level: 3, years: 3, major: true, suppress: false},
+        {name: 'Terraform', level: 3, years: 4, major: true, suppress: false},
         {name: 'Ansible', level: 3, years: 4, major: true, suppress: false},
         {name: 'Puppet', level: 3, years: 4, major: false, suppress: false},
-        {name: 'AWS', level: 4, years: 4, major: true},
+        {name: 'AWS', level: 4, years: 5, major: true},
         {name: 'REDIS', level: 2, years: 3, suppress: true},
         {name: 'Influxdb', level: 2, years: 2, suppress: true},
         {name: 'Packer', level: 2, years: 2, suppress: true},
-        {name: 'Maven', level: 1, years: 8, suppress: true},
+        {name: 'Maven', level: 2, years: 8, suppress: true},
         {name: 'Javascript', level: 1, years: 5, suppress: true},
         {name: 'Rails', level: 2, years: 4, major: true, suppress: true},
-        {name: 'Python', level: 2, years: 2, major: false, suppress: false},
+        {name: 'Python', level: 2, years: 3, major: false, suppress: false},
         {name: 'Travis_CI', level: 2, years: 3, suppress: false},
-        {name: 'Grafana', level: 2, years: 3, suppress: false},
-        {name: 'Prometheus', level: 3, years: 3, major: true, suppress: false},
+        {name: 'Grafana', level: 3, years: 5, suppress: false},
+        {name: 'Prometheus', level: 4, years: 4, major: true, suppress: false},
+        {name: 'Victoria_Metrics', level: 4, years: 3, major: true, suppress: false},
         {name: 'Couchbase', level: 3, years: 1, major: false, suppress: false},
-        {name: 'Atlantis', level: 3, years: 1, major: false, suppress: false},
-        {name: 'Hibernate', level: 2, years: 2,major: false, suppress: false},
+        {name: 'Atlantis', level: 3, years: 2, major: false, suppress: false},
+        {name: 'Hibernate', level: 2, years: 2, major: false, suppress: false},
         {name: 'Elk', level: 3, years: 3, major: false, suppress: false},
         {name: 'Consul', level: 2, years: 2, major: false, suppress: false},
         {name: 'Nomad', level: 1, years: 1, major: false, suppress: false},
         {name: 'Puppet', level: 2, years: 2, suppress: false},
-        {name: 'Kubernetes', level: 2, years: 2,  major: true, suppress: false},
+        {name: 'Kubernetes', level: 4, years: 3,  major: true, suppress: false},
         {name: 'CORBA', level: 2, years: 3, tag: '+',  suppress: false},
-        {name: 'Kafka', level: 2, years: 2, tag: '+',  suppress: false}])
+        {name: 'Kafka', level: 2, years: 3, tag: '+',  suppress: false}])
 
 facade.skills = Skill.all
 
@@ -78,20 +79,21 @@ p = Position.create(
     facade: facade,
     joined:  Time.strptime("Sep 2018", "%b %Y"),
     departed:  nil,
-    company_name: "Nexmo",
-    company_website: "www.nexmo.com",
-    description: "At NexMo I am responsible for providing AWS technology knowledge, advice and support to development teams as they migrate our systems to AWS. I am also generally responsible for monitoring and instrumentation of systems in AWS and for the design and provisioning of sections of our AWS infrastructure as a whole."
+    company_name: "Vonage API Group",
+    company_website: "developer.vonage.com",
+    description: "I am responsible for providing AWS technology knowledge, advice and support to development teams as they migrate our systems to AWS. I am also generally responsible for monitoring and instrumentation of systems in AWS and for the design and provisioning of sections of our AWS infrastructure as a whole."
 )
 
-Achievement.create([{ position_id: p.id, description: "Troubleshooting performance issues in our development and production AWS environments"},
-                    { position_id: p.id, description: "Implementation of EKS as a platform for our integration with Whatsapp, and supporting it as it grew to over a million messages a day"},
-                    { position_id: p.id, description: "Implementation of Prometheus and Victoria Metrics across various platforms."},
-                    { position_id: p.id, description: "Development of standalone Ruby applications and Ruby lambdas for instrumentation and systems management."},
+Achievement.create([{ position_id: p.id, description: "Implementation of EKS as a platform for our integration with Whatsapp, and supporting it as it grew to over two million messages a day"},
+                    { position_id: p.id, description: "Design and Implementation of our VictoriaMetrics monitoring ecosystem."},
+                    { position_id: p.id, description: "Development of Ruby applications and lambdas for instrumentation and systems management."},
                     { position_id: p.id, description: "Automation of Couchbase cluster creation and XDCR establishment both within and across Cloud providers."},
-                    { position_id: p.id, description: "Implementation of Atlantis as a CI/CD pipeline for terraform pull requests in AWS."}
+                    { position_id: p.id, description: "Implementation of Atlantis as a CI/CD pipeline for terraform pull requests in AWS."},
+                    { position_id: p.id, description: "Implementation of ArgoCD as a CI/CD pipeline for Kubernetes GitOps."},
+                    { position_id: p.id, description: "Multiple \"key contributor\" awards."}
                    ])
 
-p.skills = Skill.where(name: ['Java', 'Ruby', 'AWS', 'Terraform', 'Puppet', 'Lambda', 'Jenkins', 'EKS', 'Prometheus', 'Grafana', 'Elk', 'Couchbase', 'Atlantis', 'Kafka'])
+p.skills = Skill.where(name: ['Java', 'Ruby', 'AWS', 'Terraform', 'Puppet', 'Lambda', 'ArgoCD', 'EKS', 'VictoriaMetrics', 'Grafana', 'Elk', 'Couchbase', 'Atlantis', 'Kafka'])
 p.save
 
 p = Position.create(
@@ -102,11 +104,8 @@ p = Position.create(
                 company_name: "CurrencyCloud",
                 company_website: "www.currencycloud.com",
                 description: "In June 2016 I was promoted to Head of Engineering, Systems Capability at Currency Cloud.\n
-My remit was the performance, availability and scalability of our software systems as a whole, from front-ends and
-our APIs down to the database and AMQP level.\n
-This involved guidance and oversight during the design process; reviewing, extending and performance-tuning
-service level code and support libraries used by the feature delivery teams, as well as ensuring that we captured
-usable metrics via statsd and logstash."
+My remit was the performance, availability and scalability of our software systems as a whole, from front-ends and our APIs down to the database and AMQP level.\n
+This involved guidance and oversight during the design process; reviewing, extending and performance-tuning service level code and support libraries used by the feature delivery teams, as well as ensuring that we captured usable metrics via statsd and logstash."
 )
 
 Achievement.create([{
@@ -127,13 +126,8 @@ p = Position.create(
     departed:  Time.strptime("Jun 2016", "%b %Y"),
     company_name: "CurrencyCloud",
     company_website: "www.currencycloud.com",
-    description: "Software engineering and systems design, centered around modernising a fragmented codebase and providing
-scalable solutions.
-I was the primary Java technical developer, and worked on integrating various java technologies into our JRuby
-stack and on troubleshooting existing legacy java systems such as the communications layer of our legacy FIX
-backbone.
-I managed a team of four people comprising two developers and two testers; our focus and area of expertise is
-the FX section of our platform."
+    description: "Software engineering and systems design, centered around modernising a fragmented codebase and providing scalable solutions. I was the primary Java technical developer, and worked on integrating various java technologies into our JRuby stack and on troubleshooting existing legacy java systems such as the communications layer of our legacy FIX backbone.\n
+I managed a team of four people comprising two developers and two testers; our focus and area of expertise was the FX section of our platform."
 )
 
 Achievement.create([{position_id: p.id, description: "Re-engineered our event-driven email notification platform"},
@@ -154,10 +148,7 @@ p = Position.create(
     departed:  Time.strptime("Jun 2012", "%b %Y"),
     company_name: "The Travel Corporation",
     company_website: "www.ttc.com",
-    description: "Software engineering and troubleshooting of a massive legacy EJB / MDB system which drove Travel
-Corporation's booking systems. During my time at Travel Corporation I drove a systematic analysis process of the
-codebase and made recommendations on various strategies we could attempt to adopt in order to rectify the
-severe codebase bloat the system had accumulated"
+    description: "Software engineering and troubleshooting of a massive legacy EJB / MDB system which drove Travel Corporation's booking systems. During my time at Travel Corporation I drove a systematic analysis process of the codebase and made recommendations on various strategies we could attempt to adopt in order to rectify the severe codebase bloat the system had accumulated"
 )
 
 Achievement.create([{
@@ -178,8 +169,7 @@ p = Position.create(
     departed:  Time.strptime("Jul 2011", "%b %Y"),
     company_name: "Soliditech (Pty) Ltd.",
     company_website: "www.soliditech.com",
-    description: "My responsibilities included general new feature development and legacy support for our customers in Kenya,
-Tanzania and South Africa."
+    description: "My responsibilities included general new feature development and legacy support for our customers in Kenya, Tanzania and South Africa."
 )
 
 Achievement.create([{
